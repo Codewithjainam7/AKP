e02426899204fd7c43fdf3807b5c887e7ab87482
Jainam Jain <jainjainam@412gmail.com>
2026-05-01 05:13:50 +0530

    UI: Enhanced orange liquid background visibility with darker shades and higher opacity

--- src/components/LiquidBackground.jsx
+++ src/components/LiquidBackground.jsx
@@ -8,14 +8,14 @@ const LiquidBackground = () => {
       <div className="absolute inset-0 bg-gradient-to-br from-orange-50/40 via-transparent to-primary-50/20"></div>
 
       {/* Animated Blobs */}
-      <div className="absolute top-[-15%] left-[-10%] w-[60%] h-[60%] bg-primary-600/[0.04] blur-[120px] rounded-full animate-blob"></div>
-      <div className="absolute bottom-[-15%] right-[-10%] w-[70%] h-[70%] bg-orange-400/[0.05] blur-[120px] rounded-full animate-blob animation-delay-2000"></div>
-      <div className="absolute top-[30%] right-[-5%] w-[50%] h-[50%] bg-primary-500/[0.03] blur-[100px] rounded-full animate-blob animation-delay-4000"></div>
-      <div className="absolute bottom-[20%] left-[5%] w-[40%] h-[40%] bg-orange-300/[0.03] blur-[100px] rounded-full animate-blob animation-delay-2000"></div>
+      <div className="absolute top-[-15%] left-[-10%] w-[65%] h-[65%] bg-[#ea580c]/[0.1] blur-[100px] rounded-full animate-blob"></div>
+      <div className="absolute bottom-[-15%] right-[-10%] w-[75%] h-[75%] bg-[#c2410c]/[0.12] blur-[120px] rounded-full animate-blob animation-delay-2000"></div>
+      <div className="absolute top-[30%] right-[-5%] w-[55%] h-[55%] bg-[#f97316]/[0.08] blur-[100px] rounded-full animate-blob animation-delay-4000"></div>
+      <div className="absolute bottom-[20%] left-[5%] w-[45%] h-[45%] bg-[#ea580c]/[0.08] blur-[100px] rounded-full animate-blob animation-delay-2000"></div>
       
       {/* Subtle floating rings/dots for extra texture */}
-      <div className="absolute top-1/4 left-1/3 w-2 h-2 bg-primary-600/10 rounded-full animate-pulse"></div>
-      <div className="absolute bottom-1/4 right-1/4 w-3 h-3 bg-orange-500/10 rounded-full animate-pulse animation-delay-2000"></div>
+      <div className="absolute top-1/4 left-1/3 w-2 h-2 bg-[#ea580c]/20 rounded-full animate-pulse"></div>
+      <div className="absolute bottom-1/4 right-1/4 w-3 h-3 bg-[#c2410c]/20 rounded-full animate-pulse animation-delay-2000"></div>
     </div>
   );
 };
